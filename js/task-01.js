2f2ee8d4cc8294809feb6666f550e8e93f1f0057
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2023-09-25 00:59:18 +0300

    chang task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,15 @@
 const categoriesList = document.querySelector("#categories");
-const categoryItems = categoriesList.querySelectorAll(".item");
 
-console.log(`Number of categories: ${categoryItems.length}`);
+const childNodesArray = Array.from(categoriesList.childNodes);
 
-categoryItems.forEach((category) => {
-  const categoryName = category.querySelector("h2").textContent;
-  const categoryElements = category.querySelectorAll("li").length;
+childNodesArray.forEach((node) => {
+  if (node.nodeType === 1) {
+    const categoryName = node.querySelector("h2").textContent;
+    const categoryElements = node.querySelectorAll("li").length;
 
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${categoryElements}`);
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${categoryElements}`);
+  }
 });
+
+console.log(`Number of categories: ${childNodesArray.length}`);
